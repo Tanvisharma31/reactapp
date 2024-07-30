7d65e3d1f3b84c940ab14e9ff701f53e97a194b8
tanvisharma31 <tanvi.sharma2021@sait.ac.in>
2024-07-30 19:09:09 +0530

    modified:   src/Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,15 +6,13 @@ import PasswordResetPage from './pages/PasswordResetPage';
 import LearnModule from './user-dashboard/LearningModule/LearnModule';
 const Routing = () => {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/password-reset" element={<PasswordResetPage />} />
-        <Route path="/main" element={<Main />} />
+        <Route path="/mainApp" element={<Main />} />
         <Route path="/learnmodules" element={<LearnModule/>} />
         <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>
-    </BrowserRouter>
   );
 };
 export default Routing;
